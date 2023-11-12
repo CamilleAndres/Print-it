2b4187751b56da84525487abd348de37a8a8f6d6
CamilleAndres <camille.andrescousinou@gmail.com>
2023-11-12 09:59:26 +0100

    UpdateBanner

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,49 @@ const slides = [
 	}
 ]
 
+// Élements du DOM
 const arrowRight = document.getElementById("arrow_right");
 const arrowLeft = document.getElementById("arrow_left");
-const dots = document.querySelectorAll('.dots');
+const dots = document.querySelectorAll('.dot');
+const bannerImg = document.querySelector('.banner-img');
+const tagLine = document.querySelector("#banner p");
 
+// Index du slide
+let i = 0
+
+
+// Fonction pour afficher le slide
+function showSlide(Index) {
+	if (i >= slides.length) {
+		return;
+	}
+}
+
+// Carrousel images et textes
+bannerImg.src = "./assets/images/slideshow/" + slides[i].image;
+tagLine.innerHTML = slides[i].tagLine;
+
+// Carrousel dots
+for (let dot_selected = 0; dot_selected < 4; dot_selected ++){
+	console.log(dot_selected)
+}
+
+// Clique fléche gauche
+arrowLeft.addEventListener("click", () => {
+	const prev = i -1;
+	if (prev < 0) {
+		showSlide(slides.length -1);
+	} else {
+		showSlide(prev);
+	}
+})
+
+// Clique fléche droite
+arrowRight.addEventListener("click", function () {
+	const next = i +1;
+	if (next >= slides.length) {
+		showSlide(0);
+	} else {
+		showSlide(next);
+	}
+})
